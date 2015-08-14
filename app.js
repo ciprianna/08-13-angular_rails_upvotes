84245f8a0c32acd08eb06eb14f6f35a04d242123
Ciprianna <ciprianna17@gmail.com>
2015-08-14 11:29:32 -0500

    Users can enter their own post title; cannot enter a blank title

--- app.js
+++ app.js
@@ -11,7 +11,9 @@ function($scope){
     {title: 'post5', upvotes: 4}
   ];
   $scope.addPost = function(){
-    $scope.posts.push({title: 'A new post!', upvotes: 0});
+    if(!$scope.title || $scope.title === '') {return;}
+    $scope.posts.push({title: $scope.title, upvotes: 0});
+    $scope.title = '';
   };
 }
 ]);
