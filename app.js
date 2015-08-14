d99b96420e80d29c679d763a5c48576f69313116
Ciprianna <ciprianna17@gmail.com>
2015-08-14 12:55:29 -0500

    Binded the posts factory to the controller.

--- app.js
+++ app.js
@@ -7,27 +7,22 @@ angular.module('flapperNews', [])
 }])
 .controller('MainCtrl', [
   '$scope',
-function($scope){
-  $scope.test = "Hello, world!";
-  $scope.posts = [
-    {title: 'post1', upvotes: 5},
-    {title: 'post2', upvotes: 2},
-    {title: 'post3', upvotes: 12},
-    {title: 'post4', upvotes: 9},
-    {title: 'post5', upvotes: 4}
-  ];
-  $scope.addPost = function(){
-    if(!$scope.title || $scope.title === '') {return;}
-    $scope.posts.push({
-      title: $scope.title,
-      link: $scope.link,
-      upvotes: 0
-    });
-    $scope.title = '';
-    $scope.link = '';
-  };
-  $scope.incrementUpvotes = function(post){
-    post.upvotes += 1;
+  'posts',
+  function($scope, posts){
+    $scope.test = "Hello, world!";
+    $scope.posts = posts.posts;
+    $scope.addPost = function(){
+      if(!$scope.title || $scope.title === '') {return;}
+      $scope.posts.push({
+        title: $scope.title,
+        link: $scope.link,
+        upvotes: 0
+      });
+      $scope.title = '';
+      $scope.link = '';
+    };
+    $scope.incrementUpvotes = function(post){
+      post.upvotes += 1;
+    }
   }
-}
 ]);
