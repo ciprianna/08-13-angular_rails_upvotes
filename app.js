4693c576f7d7f8ba719060b93250d963f81b8fdd
Ciprianna <ciprianna17@gmail.com>
2015-08-14 11:16:35 -0500

    Added posts with manually-written upvotes. Displays the votes in   descending order by votes to view.

--- app.js
+++ app.js
@@ -3,5 +3,12 @@ angular.module('flapperNews', [])
   '$scope',
 function($scope){
   $scope.test = "Hello, world!";
+  $scope.posts = [
+    {title: 'post1', upvotes: 5},
+    {title: 'post2', upvotes: 2},
+    {title: 'post3', upvotes: 12},
+    {title: 'post4', upvotes: 9},
+    {title: 'post5', upvotes: 4}
+  ];
 }
 ]);
